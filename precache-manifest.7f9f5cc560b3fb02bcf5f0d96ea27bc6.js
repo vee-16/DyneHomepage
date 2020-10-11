0fe2b82143a3a0271f161e6e68728fe0076f1692
vee-16 <59987667+vee-16@users.noreply.github.com>
2020-10-11 04:13:41 +0400

    Updates

---
+++ precache-manifest.7f9f5cc560b3fb02bcf5f0d96ea27bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3700619c3b0060062be958a665534f25",
+    "url": "/DyneHomepage/index.html"
+  },
+  {
+    "revision": "937ea8d2fad799ca1b2e",
+    "url": "/DyneHomepage/static/css/main.d5c72297.chunk.css"
+  },
+  {
+    "revision": "4ebe5bcc3739c16d2fc1",
+    "url": "/DyneHomepage/static/js/2.8422137d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DyneHomepage/static/js/2.8422137d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937ea8d2fad799ca1b2e",
+    "url": "/DyneHomepage/static/js/main.9275429c.chunk.js"
+  },
+  {
+    "revision": "2f27addf8ea6bc1086a3",
+    "url": "/DyneHomepage/static/js/runtime-main.461bf103.js"
+  }
+]);
